f5cef38aad16793ba0bba5f894bde44b9f0d08ec
ashish singla <ashish singla>
2016-06-28 11:46:03 +0530

    changes in function of script/index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,27 @@
 //script/index.js
 
+function login(req, res) {
+  	 console.log("login"+req.body);
+     res.status(200).send(req.body);
+}
+
+function register(req, res) {
+    console.log("register"+req.body);
+    res.status(200).send(req.body);
+}
+
 module.exports = {
-  register: function(req, res) {
-    console.log(req.body);
-    res.status(200).send('got it');
+  index: function(req,res){
+  	
+  	var url=req.originalUrl.split('/');
+    console.log(url);	
+    if(url[2] === 'login') {
+    	console.log("login");
+		login(req,res);
+	}
+	else if(url[2] === 'register') {
+		console.log("register");
+		register(req,res);
+	}
   }
 };
